718abdfe5ce0a636cda960a9fdf022401769f391
Peter Schnabel <pschnabel@carekinesis.com>
2015-11-19 15:07:53 -0500

    add prpject

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+echo 'hello!';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hello</title>
+</head>
+<body>
+	<h2>Big</h2>
+	<img src="uploads/yorkie1.jpg">
+	<img src="uploads/yorkie2.jpg">
+
+	<h2>Small</h2>
+	<img src="uploads/thumbs/yorkie1.jpg">
+	<img src="uploads/thumbs/yorkie2.jpg">
+
+</body>
+</html>
